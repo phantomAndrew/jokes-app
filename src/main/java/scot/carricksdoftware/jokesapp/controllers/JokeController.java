4011e08ce0b14a344f18130fccc9dc00cc233d9e
Andrew Grant <apg@carricksoftware.co.uk>
2020-08-12 22:47:59 +0100

    Configuration example using @Configuration

---
+++ src/main/java/scot/carricksdoftware/jokesapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package scot.carricksdoftware.jokesapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import scot.carricksdoftware.jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
